f3f7b829dccfc5a980ad2856f2dfc1db711d8225
Agnik Saha <agniksaha2019@gmail.com>
2020-12-20 22:32:17 +0530

    redux thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,8 @@ import { Card, CardImg,CardBody,CardTitle,CardText,Breadcrumb,BreadcrumbItem,Row
     Button, Modal, ModalHeader, ModalBody,
     Form, FormGroup, Input, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 const maxLength = len => val => !val || val.length <= len;
@@ -158,9 +160,26 @@ class CommentForm extends Component {
 
     
 
-       const DishDetail = (props) => {
-      
-        if(props.dish != null)
+    const DishDetail = (props) => {
+        if (props.isLoading) {
+          return(
+              <div className="container">
+                  <div className="row">            
+                      <Loading />
+                  </div>
+              </div>
+          );
+      }
+      else if (props.errMess) {
+          return(
+              <div className="container">
+                  <div className="row">            
+                      <h4>{props.errMess}</h4>
+                  </div>
+              </div>
+          );
+      }
+      else if (props.dish != null) 
         return (
             <div className="container">
             <div className="row">
